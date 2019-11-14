91a1324bf4b0f2a3e565abd07e6a42427b6a0cb7
kevinsw21 <55318663+kevinsw21@users.noreply.github.com>
2019-11-14 15:21:42 +0700

    UGD 11 vue

---
+++ src/components/dashboardContents/landingController.vue
@@ -0,0 +1,68 @@
+<template>
+  <section
+    id="welcome"
+    class="hide-overflow"
+  >
+    <v-layout>
+      <v-flex
+        hidden-sm-and-down
+        md6
+      >
+        <v-img
+          src="https://cdn.vox-cdn.com/thumbor/y_zQVlSf3T9JXNsdTVZ-V1lDZCc=/0x227:1688x1688/1200x675/filters:focal(731x663:1001x933)/cdn.vox-cdn.com/uploads/chorus_image/image/60222205/InfinityWar5aabd55fed5fa.0.jpg"
+          height="100vh"
+        />
+      </v-flex>
+
+      <v-flex
+        xs12
+        md6
+        align-content-space-between
+        layout
+        :pa-5="$vuetify.breakpoint.smAndDown"
+        wrap
+      >
+        <base-bubble-1
+          style="transform: rotate(180deg) translateY(25%)"
+        />
+
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-flex
+            xs10
+            md6
+          >
+            <base-heading><h1>Welcome!</h1></base-heading>
+            <br>
+            <base-text>
+            <p style="text-align:justify">
+              Welcome
+              Welcome
+              Welcome
+              Welcome
+              Welcome
+              Welcome
+              Welcome
+              Welcome
+              WelcomeWelcome
+              WelcomeWelcome
+              Welcome
+              Welcome
+              
+            </p>
+            </base-text>
+            <base-btn class="mt-4">
+                <v-btn rounded color="primary" dark router to="/user">Get In</v-btn>
+            </base-btn>
+          </v-flex>
+        </v-layout>
+
+        <base-bubble-2
+          style="transform: rotate(180deg) translate(-200px, -15%)"
+        />
+      </v-flex>
+    </v-layout>
+  </section>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue')
+function loadView(view) 
+{
+    return () => import(/* webpackChunkName: "view-[request]" */ `../components/dashboardContents/${view}.vue`)
+    }
+
+    const routes = [
+        {
+            path: '/',
+            component: DashboardLayout,
+            children: [
+                {
+                    name: 'UserController',
+                    path: '/user',
+                    component: loadView('userController')
+                },
+                {
+                    name: 'ServicesController',
+                    path: '/services',
+                    component: loadView('servicesController')
+                },
+                {
+                    name: 'LandingController',
+                    path: '',
+                    component: loadView('landingController')
+                }
+            ]
+        },
+    ]
+
+Vue.use(Router)
+const router = new Router({ mode: 'history', routes: routes })
+export default router
